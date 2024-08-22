72ef0a99acb2c42247175f2b208b356555b0c2b8
Muhammad Imran <muhammadimrann80055@gmail.com>
2024-08-22 16:40:55 +0500

    last commit

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,13 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  center: listing.geometry.coordinates, // starting position [lng, lat]
+  center: [67.02053, 24.855368], // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
 
 // Create a default Marker and add it to the map.
 const marker1 = new mapboxgl.Marker()
-  .setLngLat(listing.geometry.coordinates)
+  .setLngLat([67.02053, 24.855368])
   .setPopup(
     new mapboxgl.Popup({ offset: 25 }).setHTML(
       `<h5>${listing.title}</h5><p>Exact Location will be provided after booking</p>`
